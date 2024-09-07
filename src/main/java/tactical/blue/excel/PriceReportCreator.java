2453de1fea11f0cf1b0716bfc460ad2cc987bba9
Argel Hernandez <argel6767@gmail.com>
2024-09-07 15:08:40 -0400

    adds working Bound Tree parsing

--- src/main/java/tactical/blue/excel/PriceReportCreator.java
+++ src/main/java/tactical/blue/excel/PriceReportCreator.java
@@ -94,7 +94,7 @@ public class PriceReportCreator{
    private void readCSVFiles() throws IOException {
         System.out.println("readCSVFiles() was called");
 
-        // Step 1: Read Octoparse CSV into a map (URL -> row data)
+        // Step 1: Read Web Scrape Data CSV into a map (URL -> row data)
         Map<String, List<String[]>> webScrapedMap = new HashMap<>();
             String currLineOctoparse;
             int iteration = 0;
@@ -164,6 +164,9 @@ public class PriceReportCreator{
             case "Bound Tree":
                 List<BoundTreeExcelRow> boundTreeRows = boundTreeExcelRow(currItemArray, webScrapedMap);
                 if (boundTreeRows != null) {
+                    for (BoundTreeExcelRow row : boundTreeRows) {
+                        System.out.println(row.toString());
+                    }
                     rows.addAll(boundTreeRows);
                 }
             case "Medco":
@@ -219,23 +222,41 @@ public class PriceReportCreator{
             List<String[]> urlValList = webScrapedMap.get(itemUrl); //grabs all products found under url
 
             for (String[] currWebScrapedDataArray : urlValList) { //makes new objects for each one
-                String customerDescription = currItemArray[0]; //objects with same URL with have the same customer description
-                String itemName = currWebScrapedDataArray[columnHeaderIndex.get("Product")];
-                String manufacturer = currWebScrapedDataArray[columnHeaderIndex.get("Manufacturer")];
-                String sku = currWebScrapedDataArray[columnHeaderIndex.get("SKU")];
-                int quantityRequested = Integer.parseInt(currItemArray[1]);
-                String packaging = currWebScrapedDataArray[columnHeaderIndex.get("Packaging")];
-                double msrp = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("List_Price")]);
-                double wholesalePrice = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("Wholesale")]);
+                
+                    /*
+                     * Grab all scraped Info to make row objects
+                     * 
+                     */
+                    String customerDescription = currItemArray[0]; //objects with same URL with have the same customer description
+                    String itemName = currWebScrapedDataArray[columnHeaderIndex.get("Product")];
+                    String manufacturer = currWebScrapedDataArray[columnHeaderIndex.get("Manufacturer")].trim();
+                    String sku = currWebScrapedDataArray[columnHeaderIndex.get("SKU")];
+                    int quantityRequested = Integer.parseInt(currItemArray[1]);
+                    String packaging = currWebScrapedDataArray[columnHeaderIndex.get("Packaging")];
+                    double msrp = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("List_Price")]);
+                    double wholesalePrice = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("Wholesale")]);
 
-                //Grab bulk items to make another BoundTreeExcelRow object
-                double wholesaleBulk = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("Whole_Sale_Bulk")]);
-                double msrpBulk = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("List_Price_Bulk")]);
+                    //Grab bulk items to make another BoundTreeExcelRow object
+                    /*
+                     * Add both a regular and bulk item row to thw entire list of rows
+                     */
+                    try {
+                        double wholesaleBulk = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("Whole_Sale_Bulk")]);
+                        double msrpBulk = Double.parseDouble(currWebScrapedDataArray[columnHeaderIndex.get("List_Price_Bulk")]);
+                        String packagingBulk = currWebScrapedDataArray[columnHeaderIndex.get("Bulk_Packaging")];
+                        productRows.add(new BoundTreeExcelRow(customerDescription, itemName, manufacturer, sku, quantityRequested, packaging, msrp, wholesalePrice, itemUrl)); 
+                        productRows.add(new BoundTreeExcelRow(customerDescription, itemName, manufacturer, sku, quantityRequested, packagingBulk, msrpBulk, wholesaleBulk, packaging)); 
 
-                productRows.add(new BoundTreeExcelRow(customerDescription, itemName, manufacturer, sku, quantityRequested, packaging, msrp, wholesalePrice, itemUrl)); //then add current row to all the will be returned
-                productRows.add(new BoundTreeExcelRow(customerDescription, itemName, manufacturer, sku, quantityRequested, packaging, msrpBulk, wholesaleBulk, packaging)); //also add bulk row
+                    }
+                    /*
+                     * Will catch NullPointerException that is thrown when an item has no bulk info
+                     */
+                    catch(NullPointerException npe) {
+                        productRows.add(new BoundTreeExcelRow(customerDescription, itemName, manufacturer, sku, quantityRequested, packaging, msrp, wholesalePrice, itemUrl)); 
+                    }
+
             }
-            
+                
             return productRows;
         }
     }
@@ -357,7 +378,7 @@ public class PriceReportCreator{
         }
 
         public static void main(String[] args) {
-            PriceReportCreator cleanExcelFile = new PriceReportCreator("/Users/argelhernandezamaya/Desktop/Blue-Tactical/product-orders/Medco-Sports-Medicine-Scrape.csv", "/Users/argelhernandezamaya/Desktop/Blue-Tactical/product-orders/Medco-order-example.csv", "Medco");
+            PriceReportCreator cleanExcelFile = new PriceReportCreator("/Users/argelhernandezamaya/Desktop/Blue-Tactical/product-orders/Bound-Tree-Scrape.csv", "/Users/argelhernandezamaya/Desktop/Blue-Tactical/product-orders/Bound-Tree-Order-Example.csv", "Bound Tree");
             cleanExcelFile.makeNewExcelFile();
         }
 
