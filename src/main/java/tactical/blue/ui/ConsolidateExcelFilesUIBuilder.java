6c19d2d1138948393b5871d02ae5b9f46f23cd9e
Argel Hernandez <argel6767@gmail.com>
2024-09-11 12:56:59 -0400

    creates mutli file chooser

--- src/main/java/tactical/blue/ui/ConsolidateExcelFilesUIBuilder.java
+++ src/main/java/tactical/blue/ui/ConsolidateExcelFilesUIBuilder.java
@@ -1,13 +1,22 @@
 package tactical.blue.ui;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import java.io.FileNotFoundException;
+import java.io.File;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import tactical.blue.navigation.UINavigation;
 
 public class ConsolidateExcelFilesUIBuilder extends UIElements{
+
+    private List<File> excelFilesUploaded = new ArrayList<>();
 
     public ConsolidateExcelFilesUIBuilder(UINavigation uiNavigation, Stage primaryStage) throws FileNotFoundException {
         super(uiNavigation);
@@ -20,10 +29,20 @@ public class ConsolidateExcelFilesUIBuilder extends UIElements{
         return super.getScene();
     }
 
-    private void build(Stage primaryStage) throws FileNotFoundException {
-        HBox hBox = new HBox(createLogoImageView());
+    private FileChooser mutitpleFilesChooser() {
+        FileChooser filesChooser = new FileChooser();
+        return filesChooser;
+    }
 
-        super.setScene(new Scene(hBox));
+    private Button createMulitpleFilesButton() {
+        Button button = new Button("Upload Files");
+    }
+
+    private void build(Stage primaryStage) throws FileNotFoundException {
+        VBox vBox = new VBox(createLogoBox(), createGoBackAndEndProgramButtonsHBox());
+        vBox.setSpacing(20);
+
+        super.setScene(new Scene(vBox, getPageWidth(), getPageHeight()));
     }
 
 }
