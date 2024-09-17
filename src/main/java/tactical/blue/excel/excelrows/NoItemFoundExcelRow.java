edfabb01e959dd7e0fa833d4570938de2a35cc85
Argel Hernandez <argel6767@gmail.com>
2024-09-17 09:42:33 -0400

    begins no result found logic

---
+++ src/main/java/tactical/blue/excel/excelrows/NoItemFoundExcelRow.java
@@ -0,0 +1,10 @@
+package tactical.blue.excel.excelrows;
+
+
+/*
+ * TODO Implement this class and use it for parsing algorithms
+ * If no item is found, an object of this class will be instantiated, as opposed to whichever site object that would have been made
+ */
+public class NoItemFoundExcelRow extends ExcelRow{
+
+}
