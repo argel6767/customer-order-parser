3ebc1d6abb6a2c4084b4ad5845825b9c562fd885
argel6767 <argel6767@gmail.com>
2024-10-26 13:12:14 -0400

    add TODOs

--- src/main/java/tactical/blue/async/AsyncPriceReportManager.java
+++ src/main/java/tactical/blue/async/AsyncPriceReportManager.java
@@ -1,8 +1,11 @@
 package tactical.blue.async;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /*
@@ -24,13 +27,20 @@ public class AsyncPriceReportManager {
         this.executor = executor;
     }
 
-    public void runCSVParsingConcurrently() {
+    /*
+     * TODO make this method once the bottom two are fixed!!!
+     *  general format : take the two functions in and then do their work and once the two are done do the creation of the Price Report!!!
+     */
+    public void runCSVParsingConcurrently(Function<Void, CompletableFuture<HashMap<String, List<String>>>> mapScrapedRowsTask,
+                                          Function<Void, List<String[]>> getOrderInfoRows,
+                                          BiConsumer<HashMap<String, List<String>>, List<String[]>> parseScrapedRowsToExcelRowsTask) {
 
     }
 
     /*
      * submits the task to the ExecutorService
      */
+    //TODO Replace this with supplyAsync, runAsync does not return a value!!!
     public CompletableFuture<?> doCSVParseTaskAsync(Function<Void, CompletableFuture<?>> function) {
         return CompletableFuture.runAsync(createCSVParsingRunnable(function), executor);
     }
@@ -38,6 +48,7 @@ public class AsyncPriceReportManager {
     /*
      * creates the Runnable to run Async, using a Function allows for either task to work
      */
+    //TODO replace with a Supplier, Runnable does not return the value needed!!!
     private Runnable createCSVParsingRunnable(Function<Void, CompletableFuture<?>> function) {
         return () -> {
             function.apply(null);
