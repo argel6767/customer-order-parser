1f82e0d71ba2fc2c3d5c8a4aae1f44abf78e632e
Argel Hernandez <argel6767@gmail.com>
2024-10-28 17:37:03 -0400

    write AsyncPriceReportManager

--- src/main/java/tactical/blue/async/AsyncPriceReportManager.java
+++ src/main/java/tactical/blue/async/AsyncPriceReportManager.java
@@ -7,12 +7,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /*
  * This class holds the managing of tasks that will happen concurrently in PriceReportCreator
  * ie reading both CSV files at the same time for greater efficiency
  */
-public class AsyncPriceReportManager {
+public class AsyncPriceReportManager<T> {
 
     private final ExecutorService executor;
 
@@ -31,27 +32,18 @@ public class AsyncPriceReportManager {
      * TODO make this method once the bottom two are fixed!!!
      *  general format : take the two functions in and then do their work and once the two are done do the creation of the Price Report!!!
      */
-    public void runCSVParsingConcurrently(Function<Void, CompletableFuture<HashMap<String, List<String>>>> mapScrapedRowsTask,
-                                          Function<Void, List<String[]>> getOrderInfoRows,
+    public void runCSVParsingConcurrently(Function<Void, HashMap<String, List<String>>> mapScrapedRows, Function<Void, List<String[]>> getOrderInfoRows,
                                           BiConsumer<HashMap<String, List<String>>, List<String[]>> parseScrapedRowsToExcelRowsTask) {
-
+        CompletableFuture<HashMap<String, List<String>>> mapScrapedRowsTask = doCSVParseTaskAsync(mapScrapedRows);
+        CompletableFuture<List<String[]>> getOrderInfoRowsTask = doCSVParseTaskAsync(getOrderInfoRows);
+        CompletableFuture<Void> parsingDone = mapScrapedRowsTask.thenAcceptBoth(getOrderInfoRowsTask, parseScrapedRowsToExcelRowsTask);
     }
 
     /*
      * submits the task to the ExecutorService
      */
-    //TODO Replace this with supplyAsync, runAsync does not return a value!!!
-    public CompletableFuture<?> doCSVParseTaskAsync(Function<Void, CompletableFuture<?>> function) {
-        return CompletableFuture.runAsync(createCSVParsingRunnable(function), executor);
+    public <T> CompletableFuture<T> doCSVParseTaskAsync(Function<Void, T> function) {
+        return CompletableFuture.supplyAsync(() -> function.apply(null), executor);
     }
 
-    /*
-     * creates the Runnable to run Async, using a Function allows for either task to work
-     */
-    //TODO replace with a Supplier, Runnable does not return the value needed!!!
-    private Runnable createCSVParsingRunnable(Function<Void, CompletableFuture<?>> function) {
-        return () -> {
-            function.apply(null);
-        };
-    }
 }
