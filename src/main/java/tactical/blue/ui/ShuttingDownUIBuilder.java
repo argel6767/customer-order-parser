d71da62e65c4acd2393b3082337461d26424547a
Argel Hernandez <argel6767@gmail.com>
2024-10-30 14:10:38 -0400

    add shutting down screen

---
+++ src/main/java/tactical/blue/ui/ShuttingDownUIBuilder.java
@@ -0,0 +1,26 @@
+package tactical.blue.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import tactical.blue.async.UIThreadExecutor;
+import tactical.blue.navigation.UINavigation;
+
+public class ShuttingDownUIBuilder extends UIComponents{
+
+    public ShuttingDownUIBuilder(UINavigation uiNavigation, UIThreadExecutor handler, Stage primaryStage) {
+        super(uiNavigation, handler);
+        build(primaryStage);
+    }
+
+    private void build(Stage primaryStage) {
+        Text text = new Text("Shutting Down...");
+        VBox container = new VBox(text);
+        container.setStyle(getContainerStyle());
+        container.setAlignment(Pos.CENTER);
+        super.setContainer(container);
+        super.setScene(new Scene(container, getPageWidth(), getPageHeight()));
+    }
+}
